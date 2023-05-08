e5c19ac36cd3e305ca8d405b22f1fb06c60c7f0f
wilsomrm2 <wilsomrm2@yahoo.com>
2023-05-09 06:10:30 +1200

    Create GetLogs.py
    This requires Mosquitto (Client and Python Library) to be installed on the server

---
+++ GetLogs.py
@@ -0,0 +1,24 @@
+import subprocess
+import paho.mqtt.publish as publish
+
+# Define the MQTT broker details
+broker_address = "localhost"
+broker_port = 1883
+broker_username = "admin"
+broker_password = "password"
+mqtt_topic = "/log/ufw"
+
+
+# Open the ufw.log file for tailing
+tail_process = subprocess.Popen(["tail", "-F", "/var/log/ufw.log"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Continuously read lines from the tail process and publish them to MQTT
+i=0
+while True:
+        print("start")
+        while True:
+                line = tail_process.stdout.readline().decode().strip()
+                if line:
+                        print(line)
+                #i+=1
+                        publish.single(mqtt_topic, line, hostname=broker_address, port=broker_port)
